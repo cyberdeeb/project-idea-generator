b57ef36a5e06920ecd234a961548d2680919d433
cyberdeeb <abrahamdeeb@gmail.com>
2025-07-27 21:48:10 -0700

    Update API endpoint in generateProjects function to use production URL

--- project-idea-generator-frontend/src/utils/openai.js
+++ project-idea-generator-frontend/src/utils/openai.js
@@ -6,12 +6,15 @@ export async function generateProjects({
   time,
   experienceLevel,
 }) {
-  const res = await axios.post('http://localhost:3001/api/generate', {
-    skills,
-    interests,
-    time,
-    experienceLevel,
-  });
+  const res = await axios.post(
+    'https://project-idea-generator.onrender.com/api/generate',
+    {
+      skills,
+      interests,
+      time,
+      experienceLevel,
+    }
+  );
 
   return res.data.content;
 }
